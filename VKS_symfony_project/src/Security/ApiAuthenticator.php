84df5ed72d9735db3825011b2f9d03e33e53dc49
maxime <maximekets80@gmail.com>
2024-04-15 16:50:24 -0400

    feat : add ApiAuthenticator.php for get user by ApiToken

---
+++ VKS_symfony_project/src/Security/ApiAuthenticator.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+
+class ApiAuthenticator extends AbstractAuthenticator
+{
+    public function supports(Request $request): ?bool
+    {
+        return $request->headers->has('Authorization') && !empty($request->headers->get('Authorization'));
+    }
+
+    public function authenticate(Request $request): SelfValidatingPassport
+    {
+        $apiToken = $request->headers->get('Authorization');
+        if (null === $apiToken) {
+            throw new CustomUserMessageAuthenticationException('No API token provided');
+        }
+
+        return new SelfValidatingPassport(new UserBadge($apiToken));
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        return null;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    {
+        return new JsonResponse(['message' => 'Authentication Failed.'], Response::HTTP_UNAUTHORIZED);
+    }
+}
